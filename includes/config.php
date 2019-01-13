5c599609cf438540cecf473df2df949a577bbf15
akmjoe <42101404+akmjoe@users.noreply.github.com>
2019-01-13 13:15:09 -0500

    Delete config.php

--- includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($handler) || !$handler) die("access denied!");
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'rothrock');
-define('DB_NAME', 'bankregister');
